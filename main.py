2e5ec9f9c7fbaf3599dd4c661741ba0b52f57336
Rahul24325 <88277388+Rahul24325@users.noreply.github.com>
2025-10-29 04:17:16 +0530

    Change BOT_TOKEN to hardcoded value
    Replaced BOT_TOKEN retrieval from environment variable with hardcoded value.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-print("DEBUG — BOT_TOKEN is:", os.environ.get("BOT_TOKEN"))
 import telebot
 from telebot import types
 import time
@@ -8,7 +6,7 @@ import os
 from keep_alive import keep_alive
 from flask import Flask, request
 
-BOT_TOKEN = os.environ.get('8325375868:AAEAvYZK_QqdYiF8R3ilgRdKRuXBzEwO78Y')
+BOT_TOKEN = 8325375868:AAEAvYZK_QqdYiF8R3ilgRdKRuXBzEwO78Y
 
 if not BOT_TOKEN:
     print("❌ ERROR: BOT_TOKEN not found in environment variables!")
