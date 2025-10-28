93b64278335a3178fd1c9dd856d7011ca4f3238f
Rahul24325 <88277388+Rahul24325@users.noreply.github.com>
2025-10-29 04:23:05 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,2 @@
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "✅ MoneyMatrix Bot is alive and running!"
-
-def run():
-    app.run(host='0.0.0.0', port=5000)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.daemon = True
-    t.start()
+Flask
+pyTelegramBotAPI
